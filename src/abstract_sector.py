e6f361de9a1b6005abe40bbf319d76573a810ddb
Juho Vainio <Juho.Vainio@student.laurea.fi>
2018-12-05 12:28:13 +0200

    Abstractish datatype to user-interface with baboons

---
+++ src/abstract_sector.py
@@ -0,0 +1,48 @@
+import vertex
+
+
+class Abstract_sector:
+    def __init__(self, v0, v1, v2):
+        # Initially only hold minimum amount to form triangle
+        self.vertices = [v0, v1, v2]
+        side_num = (v1.x - v0.x)*(v2.y - v0.y) - (v2.x - v0.x)*(v1.y - v0.y)
+        if side_num > 0:  # v2 on left of line0/1
+            self.vertices.reverse()
+        elif side_num == 0:  # v2 in line with line0/1
+            print("WARNING : Formed line instead of triangle.")
+
+    def add_vertex(self, vertexn, between1, between2):
+        # Add a new vertex between vertices between1 & between2
+        # Return True if success
+        if vertexn == between1 or vertexn == between2 or between1 == between2:
+            print("ERROR : goofy arguments for add_vertex.")
+            return False  # throwing exceptions is for scrubs
+
+        b1_index = -1
+        b2_index = -1
+
+        for i in range(len(self.vertices)):
+            if vertexn == self.vertices[i]:  # vertex == has been overloaded
+                print("WARNING : add_vertex: vertex already in abstract sector.")
+                return False
+            elif between1 == self.vertices[i]:
+                if b1_index >= 0:
+                    print("ERROR : duplicate vertices in abstract sector.")
+                    return False
+                b1_index = i
+            elif between2 == self.vertices[i]:
+                if b2_index >= 0:
+                    print("ERROR : duplicate vertices in abstract sector.")
+                    return False
+                b2_index = i
+
+        if b1_index >= 0 and b2_index >= 0:
+            if b1_index+1 == b2_index:
+                self.vertices.insert(b2_index, vertexn)
+            elif b1_index == len(self.vertices-1) and b2_index == 0:
+                self.vertices.append(vertexn)
+            else:
+                print("ERROR : There is no such line in abstract sector.")
+                return False
+
+        return True
--- src/vertex.py
+++ src/vertex.py
@@ -6,5 +6,10 @@ class Vertex:
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
+
     def to_binary(self):
         return struct.pack("<HH", self.x, self.y)
