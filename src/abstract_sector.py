f47ce25be4971b622203c351db54d12a0c8a7d3a
Juho Vainio <Juho.Vainio@student.laurea.fi>
2018-12-07 13:51:06 +0200

    inversion func for asector

--- src/abstract_sector.py
+++ src/abstract_sector.py
@@ -3,8 +3,12 @@ import vertex
 
 class Abstract_sector:
     def __init__(self, v0, v1, v2):
+        
         # Initially only hold minimum amount to form triangle
         self.vertices = [v0, v1, v2]
+        # Can't find object field accessor overloading anywhere...
+        self.__ffs__inverted_sector = False  # If set to true with invert(), asector is inverted
+        
         side_num = (v1.x - v0.x)*(v2.y - v0.y) - (v2.x - v0.x)*(v1.y - v0.y)
         if side_num > 0:  # v2 on left of line0/1
             self.vertices.reverse()
@@ -46,3 +50,10 @@ class Abstract_sector:
                 return False
 
         return True
+
+    def invert(self):
+        if not self.__ffs__inverted_sector:
+            self.__ffs__inverted_sector = True
+        else:
+            self.__ffs__inverted_sector = False
+        self.vertices.reverse()
