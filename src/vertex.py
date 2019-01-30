74d0cc231ff560e29fe2f77119e41e609f4b7845
Juho Vainio <Juho.Vainio@student.laurea.fi>
2019-01-30 12:39:06 +0200

    cast vert coords to int

--- src/vertex.py
+++ src/vertex.py
@@ -3,8 +3,8 @@ import struct
 class Vertex:
     #  2 * unsigned 16-bit integer
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
 
     def __eq__(self, other):
         if self.x == other.x and self.y == other.y:
