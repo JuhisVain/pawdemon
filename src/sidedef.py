16dc769b71f4a426a06143635d8aa92efd4d8d09
Juho Vainio <Juho.Vainio@student.laurea.fi>
2019-01-31 11:32:00 +0200

    texture setters

--- src/sidedef.py
+++ src/sidedef.py
@@ -19,6 +19,15 @@ class Sidedef:
     def set_facing_sector(self, facing):
         self.facing_sector = facing
 
+    def set_upper(self, texture):
+        self.upper_tex = texture
+
+    def set_lower(self, texture):
+        self.lower_tex = texture
+
+    def set_middle(self, texture):
+        self.middle_tex = texture
+
     def to_binary(self):
         if not isinstance(self.facing_sector, int):
             print("ERROR in sidedef to binary. Facing sector not index.")
